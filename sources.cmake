3b1e9ab7ef0c21575ed8b49dcda11ebca9e02d49
rchristie95 <robson.christie1995@gmail.com>
2026-04-15 21:14:19 +0200

    Update sources.cmake

--- sources.cmake
+++ sources.cmake
@@ -1,6 +1,6 @@
 # Modify this file to change the last-modified date when you add/remove a file.
 # This will then trigger a new cmake run automatically.
-# Stage 3 observer snapshot sources added.
+# Stage 3 observer snapshot sources added. relativistic_vfx added.
 file(GLOB_RECURSE STK_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "src/*.hpp")
 file(GLOB_RECURSE STK_SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "src/*.cpp")
 file(GLOB_RECURSE STK_SHADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "data/shaders/*")
