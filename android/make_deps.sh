503b2bafb723e77b8c8df0fe362fa30511e0a69d
Robson <robson.christie1995@gmail.com>
2026-07-19 00:17:14 +0100

    Fully disable Android OpenAL module scanning

--- android/make_deps.sh
+++ android/make_deps.sh
@@ -233,6 +233,7 @@ build_deps()
                 -DOPENSL_INCLUDE_DIR="$NDK_SYSROOT/usr/include/SLES/"           \
                 -DOPENSL_ANDROID_INCLUDE_DIR="$NDK_SYSROOT/usr/include/SLES/"   \
                 -DALSOFT_ENABLE_MODULES=OFF                                    \
+                -DCMAKE_CXX_SCAN_FOR_MODULES=OFF                                \
                 -DCMAKE_C_FLAGS="-fpic -O3 -g -w" -DCMAKE_CXX_FLAGS="-fpic -O3 -g -w" &&
         make -j $(($(nproc) + 1))
         check_error
